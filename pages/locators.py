d85001f82ee8242e596cc803f2112635db1452e6
Anna Panukova <ann.panukova@yandex.ru>
2022-07-29 15:28:05 +0300

    Add tests for product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.XPATH, "//*[@id='login_form']")
     REGISTER_FORM = (By.XPATH, "//*[@id='register_form']")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.XPATH, "//*[contains(@class,'btn-add-to-basket')]")
+    PRODUCT_NAME = (By.XPATH, "//*[contains(@class,'product_main')]/h1")
+    BASKET_MESSAGE_PRODUCT_NAME = (By.XPATH, "//*[@id = 'messages']/div[1]/*/strong")
+    PRODUCT_PRICE = (By.XPATH, "//*[@class = 'price_color']")
+    BASKET_MESSAGE_PRODUCT_PRICE = (By.XPATH, "//*[@id = 'messages']/div[3]/*/*/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,23 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+        self.solve_quiz_and_get_code()
+        self.should_be_correct_name_in_basket_message()
+        self.should_be_correct_price_in_basket_message()
+
+    def should_be_correct_name_in_basket_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_product_name = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PRODUCT_NAME).text
+        assert product_name == message_product_name, f"Product name in header {product_name} doesn't match with name" \
+                                                     f"in  message {message_product_name}"
+
+    def should_be_correct_price_in_basket_message(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        message_product_price = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PRODUCT_PRICE).text
+        assert product_price == message_product_price, f"Product price in header {product_price} doesn't match with " \
+                                                       f"price in message {message_product_price} "
