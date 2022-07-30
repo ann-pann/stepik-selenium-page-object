79ac3bacba467ea60493da7b6b5c06dc27e67e93
Anna Panukova <ann.panukova@yandex.ru>
2022-07-30 17:47:49 +0300

    Fix assert messages

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,8 +5,8 @@ from pages.locators import BasketPageLocators
 class BasketPage(BasePage):
     def should_not_be_products_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCTS_FORM), \
-            "Products is presented, but should not be"
+            "Products are present, but should not be"
 
     def should_basket_be_empty(self):
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
-            "Empty basket message is presented, but should not be"
+            "Empty basket message is not present, but should be"
